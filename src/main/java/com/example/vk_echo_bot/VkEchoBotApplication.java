7a843dc2beed4489d4da73e620ff6ad9fb992a31
Nurmukhammad <n1makhmudov@hmail.com>
2024-05-30 23:38:35 +0300

    Finished project

--- src/main/java/com/example/vk_echo_bot/VkEchoBotApplication.java
+++ src/main/java/com/example/vk_echo_bot/VkEchoBotApplication.java
@@ -3,6 +3,7 @@ package com.example.vk_echo_bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//link to bot https://vk.me/join/AZQ1dxRxlgQ1B_/Ny4Ynz0Hb
 @SpringBootApplication
 public class VkEchoBotApplication {
 
