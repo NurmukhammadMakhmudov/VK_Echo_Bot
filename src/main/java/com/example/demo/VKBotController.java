cc478b80fc2d9267a4815de77235c32d92975671
NurmukhammadMakhmudov <n1makhudov@gmail.com>
2024-05-30 10:55:29 -0400

    fixed @Value, need to check code

--- src/main/java/com/example/demo/VKBotController.java
+++ src/main/java/com/example/demo/VKBotController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,12 +10,15 @@ import java.io.IOException;
 
 @RestController
 public class VKBotController {
+    @Autowired
+    VKService vkService;
 
     @PostMapping
-    public String messageReceiver(@RequestBody VKMessage message) throws IOException {
-        VKService vkService = new VKService(); // creating object of VKService to send message back to user
+    public String messageReceiver(@RequestBody VKMessage message) {
+         // creating object of VKService to send message back to user
         vkService.sendMessage(message.getObject().getMessage().getPeer_id(), message.getObject().getMessage().getText()); // passing ID of user, message to send and random_ID
-        return "ok";
+        return "OK";
+
     }
 
 
